278dfbbe1bc88dfca55a74ade1b2a8547c032166
urfa <urfa@kuafor.art>
2026-08-25 08:18:16 +0300

    Configure standalone build, htaccess and PM2 scripts

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  compress: false,
   images: {
     remotePatterns: [
       {
